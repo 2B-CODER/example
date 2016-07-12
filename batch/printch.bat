4920bbecb21be793d0476d2dea173ebbeb4c23f6
git123hub <sf2164229838@sina.com>
2016-07-12 18:31:22 +0800

    Add files via upload

---
+++ batch/printch.bat
@@ -0,0 +1,20 @@
+@echo off
+echo jS[SX,SPZ,!P_772W/(]9(]@ !SX5SL !>printc.com
+echo 502500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x00.com
+echo 582520PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0a.com
+echo 582550PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0d.com
+echo 5a25z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1b.com
+echo 5a25A0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_space.com
+echo 5a25C0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_quot.com
+echo 5a25D0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x25.com
+echo 5a25G0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_amp.com
+echo 5a25I0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x28.com
+echo 5a25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x29.com
+echo 5A25}0PPj`X,~P_0E50E=XZ/!j0X50L/!>print_lt.com
+echo 5C25}0PPj`X,~P_0E50E=XZ/!j0X50L/!>print_gt.com
+echo 5a25?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_caret.com
+echo , PP,dPZ]X1F61F?502500-!5Lo51!H-!>p_pipe.com
+echo , PP,aPZ]X1F61F?502500-!5Lo51!H-!>p_0x7f.com
+echo , PP,`PZ]X1F61F?502500-!5Lo51!H-!>p_0x80.com
+echo 503-10PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xff.com
+printc.com %1

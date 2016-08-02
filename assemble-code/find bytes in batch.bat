8c5fa697731a425a952aea3080ecc6869c821fe9
git123hub <sf2164229838@sina.com>
2016-08-02 12:59:56 +0800

    Add files via upload

---
+++ assemble-code/find bytes in batch.bat
@@ -0,0 +1,262 @@
+@echo off
+echo jS[SX,SPZ,!P_772W/(]9(]@ !SX5SL !>printc.com
+echo 502500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x00.com
+echo 512500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x01.com
+echo 522500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x02.com
+echo 532500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x03.com
+echo 542500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x04.com
+echo 552500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x05.com
+echo 562500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x06.com
+echo 572500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x07.com
+echo [XPS5! P_0E$77PZ!!5a!)E/5io6E????>p_0x08.com
+echo 592500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x09.com
+echo 582520PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0a.com
+echo 582530PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0b.com
+echo 582540PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0c.com
+echo 582550PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0d.com
+echo 582560PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0e.com
+echo 582570PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x0f.com
+echo 5a25q0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x10.com
+echo 5a25p0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x11.com
+echo 5a25s0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x12.com
+echo 5a25r0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x13.com
+echo 5a25u0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x14.com
+echo 5a25t0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x15.com
+echo 5a25w0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x16.com
+echo 5a25v0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x17.com
+echo 5a25y0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x18.com
+echo 5a25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x19.com
+echo 5b25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1a.com
+echo 5a25z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1b.com
+echo 5d25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1c.com
+echo 5e25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1d.com
+echo 5f25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1e.com
+echo 5g25x0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x1f.com
+echo 5a25A0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x20.com
+echo 5c25B0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x21.com
+echo 5a25C0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x22.com
+echo 5a25B0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x23.com
+echo 5a25E0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x24.com
+echo 5a25D0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x25.com
+echo 5a25G0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x26.com
+echo 5a25F0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x27.com
+echo 5a25I0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x28.com
+echo 5a25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x29.com
+echo 5b25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2a.com
+echo 5c25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2b.com
+echo 5d25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2c.com
+echo 5e25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2d.com
+echo 5f25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2e.com
+echo 5g25H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x2f.com
+echo 5a25Q0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x30.com
+echo 5a25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x31.com
+echo 5b25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x32.com
+echo 5c25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x33.com
+echo 5d25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x34.com
+echo 5e25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x35.com
+echo 5f25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x36.com
+echo 5g25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x37.com
+echo 5h25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x38.com
+echo 5i25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x39.com
+echo 5j25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3a.com
+echo 5k25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3b.com
+echo 5A25}0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3c.com
+echo 5m25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3d.com
+echo 5C25}0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3e.com
+echo 5o25P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x3f.com
+echo 5p2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x40.com
+echo 5q2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x41.com
+echo 5r2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x42.com
+echo 5s2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x43.com
+echo 5t2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x44.com
+echo 5u2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x45.com
+echo 5v2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x46.com
+echo 5w2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x47.com
+echo 5x2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x48.com
+echo 5x2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x49.com
+echo 5x2520PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4a.com
+echo 5x2530PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4b.com
+echo 5x2540PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4c.com
+echo 5x2550PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4d.com
+echo 5x2560PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4e.com
+echo 5x2570PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x4f.com
+echo 5a2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x50.com
+echo 5a2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x51.com
+echo 5c2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x52.com
+echo 5c2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x53.com
+echo 5e2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x54.com
+echo 5e2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x55.com
+echo 5g2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x56.com
+echo 5g2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x57.com
+echo 5i2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x58.com
+echo 5i2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x59.com
+echo 5e25?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5a.com
+echo 5d25?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5b.com
+echo 5a25=0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5c.com
+echo 5b25?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5d.com
+echo 5a25?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5e.com
+echo 5o2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x5f.com
+echo 5P2500PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x60.com
+echo 5P2510PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x61.com
+echo 5P2520PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x62.com
+echo 5P2530PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x63.com
+echo 5P2540PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x64.com
+echo 5P2550PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0x65.com
+echo , PP,zPZ]X1F61F?502500-!5Lo51!H-!>p_0x66.com
+echo , PP,yPZ]X1F61F?502500-!5Lo51!H-!>p_0x67.com
+echo , PP,xPZ]X1F61F?502500-!5Lo51!H-!>p_0x68.com
+echo , PP,wPZ]X1F61F?502500-!5Lo51!H-!>p_0x69.com
+echo , PP,vPZ]X1F61F?502500-!5Lo51!H-!>p_0x6a.com
+echo , PP,uPZ]X1F61F?502500-!5Lo51!H-!>p_0x6b.com
+echo , PP,tPZ]X1F61F?502500-!5Lo51!H-!>p_0x6c.com
+echo , PP,sPZ]X1F61F?502500-!5Lo51!H-!>p_0x6d.com
+echo , PP,rPZ]X1F61F?502500-!5Lo51!H-!>p_0x6e.com
+echo , PP,qPZ]X1F61F?502500-!5Lo51!H-!>p_0x6f.com
+echo , PP,pPZ]X1F61F?502500-!5Lo51!H-!>p_0x70.com
+echo , PP,oPZ]X1F61F?502500-!5Lo51!H-!>p_0x71.com
+echo , PP,nPZ]X1F61F?502500-!5Lo51!H-!>p_0x72.com
+echo , PP,mPZ]X1F61F?502500-!5Lo51!H-!>p_0x73.com
+echo , PP,lPZ]X1F61F?502500-!5Lo51!H-!>p_0x74.com
+echo , PP,kPZ]X1F61F?502500-!5Lo51!H-!>p_0x75.com
+echo , PP,jPZ]X1F61F?502500-!5Lo51!H-!>p_0x76.com
+echo , PP,iPZ]X1F61F?502500-!5Lo51!H-!>p_0x77.com
+echo , PP,hPZ]X1F61F?502500-!5Lo51!H-!>p_0x78.com
+echo , PP,gPZ]X1F61F?502500-!5Lo51!H-!>p_0x79.com
+echo , PP,fPZ]X1F61F?502500-!5Lo51!H-!>p_0x7a.com
+echo , PP,ePZ]X1F61F?502500-!5Lo51!H-!>p_0x7b.com
+echo , PP,dPZ]X1F61F?502500-!5Lo51!H-!>p_0x7c.com
+echo , PP,cPZ]X1F61F?502500-!5Lo51!H-!>p_0x7d.com
+echo , PP,bPZ]X1F61F?502500-!5Lo51!H-!>p_0x7e.com
+echo , PP,aPZ]X1F61F?502500-!5Lo51!H-!>p_0x7f.com
+echo , PP,`PZ]X1F61F?502500-!5Lo51!H-!>p_0x80.com
+echo , PP,_PZ]X1F61F?502500-!5Lo51!H-!>p_0x81.com
+echo ,~PZ4]P]770V10V:O!h0}X501O!??????>p_0x82.com
+echo , PP,]PZ]X1F61F?502500-!5Lo51!H-!>p_0x83.com
+echo , PP,\PZ]X1F61F?502500-!5Lo51!H-!>p_0x84.com
+echo , PP,[PZ]X1F61F?502500-!5Lo51!H-!>p_0x85.com
+echo , PP,ZPZ]X1F61F?502500-!5Lo51!H-!>p_0x86.com
+echo , PP,YPZ]X1F61F?502500-!5Lo51!H-!>p_0x87.com
+echo , PP,XPZ]X1F61F?502500-!5Lo51!H-!>p_0x88.com
+echo , PP,WPZ]X1F61F?502500-!5Lo51!H-!>p_0x89.com
+echo , PP,VPZ]X1F61F?502500-!5Lo51!H-!>p_0x8a.com
+echo , PP,UPZ]X1F61F?502500-!5Lo51!H-!>p_0x8b.com
+echo , PP,TPZ]X1F61F?502500-!5Lo51!H-!>p_0x8c.com
+echo , PP,SPZ]X1F61F?502500-!5Lo51!H-!>p_0x8d.com
+echo , PP,RPZ]X1F61F?502500-!5Lo51!H-!>p_0x8e.com
+echo , PP,QPZ]X1F61F?502500-!5Lo51!H-!>p_0x8f.com
+echo , PP,PPZ]X1F61F?502500-!5Lo51!H-!>p_0x90.com
+echo , PP,OPZ]X1F61F?502500-!5Lo51!H-!>p_0x91.com
+echo , PP,NPZ]X1F61F?502500-!5Lo51!H-!>p_0x92.com
+echo , PP,MPZ]X1F61F?502500-!5Lo51!H-!>p_0x93.com
+echo , PP,LPZ]X1F61F?502500-!5Lo51!H-!>p_0x94.com
+echo , PP,KPZ]X1F61F?502500-!5Lo51!H-!>p_0x95.com
+echo , PP,JPZ]X1F61F?502500-!5Lo51!H-!>p_0x96.com
+echo , PP,IPZ]X1F61F?502500-!5Lo51!H-!>p_0x97.com
+echo , PP,HPZ]X1F61F?502500-!5Lo51!H-!>p_0x98.com
+echo , PP,GPZ]X1F61F?502500-!5Lo51!H-!>p_0x99.com
+echo , PP,FPZ]X1F61F?502500-!5Lo51!H-!>p_0x9a.com
+echo , PP,EPZ]X1F61F?502500-!5Lo51!H-!>p_0x9b.com
+echo , PP,DPZ]X1F61F?502500-!5Lo51!H-!>p_0x9c.com
+echo , PP,CPZ]X1F61F?502500-!5Lo51!H-!>p_0x9d.com
+echo , PP,BPZ]X1F61F?502500-!5Lo51!H-!>p_0x9e.com
+echo , PP,APZ]X1F61F?502500-!5Lo51!H-!>p_0x9f.com
+echo , PP,@PZ]X1F61F?502500-!5Lo51!H-!>p_0xa0.com
+echo , PP,?PZ]X1F61F?502500-!5Lo51!H-!>p_0xa1.com
+echo ,_@PZ4BP]H770V20V;o!h0}X501o!????>p_0xa2.com
+echo , PP,=PZ]X1F61F?502500-!5Lo51!H-!>p_0xa3.com
+echo ,]@PZ4DP]H770V20V;i!h0}X501i!????>p_0xa4.com
+echo , PP,;PZ]X1F61F?502500-!5Lo51!H-!>p_0xa5.com
+echo , PP,:PZ]X1F61F?502500-!5Lo51!H-!>p_0xa6.com
+echo , PP,9PZ]X1F61F?502500-!5Lo51!H-!>p_0xa7.com
+echo , PP,8PZ]X1F61F?502500-!5Lo51!H-!>p_0xa8.com
+echo , PP,7PZ]X1F61F?502500-!5Lo51!H-!>p_0xa9.com
+echo , PP,6PZ]X1F61F?502500-!5Lo51!H-!>p_0xaa.com
+echo , PP,5PZ]X1F61F?502500-!5Lo51!H-!>p_0xab.com
+echo , PP,4PZ]X1F61F?502500-!5Lo51!H-!>p_0xac.com
+echo , PP,3PZ]X1F61F?502500-!5Lo51!H-!>p_0xad.com
+echo , PP,2PZ]X1F61F?502500-!5Lo51!H-!>p_0xae.com
+echo , PP,1PZ]X1F61F?502500-!5Lo51!H-!>p_0xaf.com
+echo , PP,0PZ]X1F61F?502500-!5Lo51!H-!>p_0xb0.com
+echo , PP,/PZ]X1F61F?502500-!5Lo51!H-!>p_0xb1.com
+echo , PP,.PZ]X1F61F?502500-!5Lo51!H-!>p_0xb2.com
+echo , PP,-PZ]X1F61F?502500-!5Lo51!H-!>p_0xb3.com
+echo , PP,,PZ]X1F61F?502500-!5Lo51!H-!>p_0xb4.com
+echo , PP,+PZ]X1F61F?502500-!5Lo51!H-!>p_0xb5.com
+echo , PP,*PZ]X1F61F?502500-!5Lo51!H-!>p_0xb6.com
+echo , PP,)PZ]X1F61F?502500-!5Lo51!H-!>p_0xb7.com
+echo , PP,(PZ]X1F61F?502500-!5Lo51!H-!>p_0xb8.com
+echo , PP,'PZ]X1F61F?502500-!5Lo51!H-!>p_0xb9.com
+echo 543-z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xba.com
+echo 553-z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xbb.com
+echo 563-z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xbc.com
+echo 573-z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xbd.com
+echo 583-z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xbe.com
+echo 583-y0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xbf.com
+echo 573-w0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc0.com
+echo 563-u0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc1.com
+echo 553-s0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc2.com
+echo 543-q0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc3.com
+echo 533-o0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc4.com
+echo 523-m0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc5.com
+echo 513-k0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc6.com
+echo 503-i0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc7.com
+echo 5/3-g0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc8.com
+echo 5.3-e0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xc9.com
+echo 5-3-c0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xca.com
+echo 5,3-a0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xcb.com
+echo 5-3-a0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xcc.com
+echo 5,3-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xcd.com
+echo 5-3-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xce.com
+echo 5.3-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xcf.com
+echo 5/3-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd0.com
+echo 503-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd1.com
+echo 513-_0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd2.com
+echo 503-]0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd3.com
+echo 503-\0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd4.com
+echo 503-[0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd5.com
+echo 503-Z0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd6.com
+echo 503-Y0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd7.com
+echo 503-X0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd8.com
+echo 503-W0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xd9.com
+echo 503-V0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xda.com
+echo 503-U0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xdb.com
+echo 503-T0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xdc.com
+echo 503-S0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xdd.com
+echo 503-R0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xde.com
+echo 503-Q0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xdf.com
+echo 503-P0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe0.com
+echo 503-O0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe1.com
+echo 503-N0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe2.com
+echo 503-M0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe3.com
+echo 503-L0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe4.com
+echo 503-K0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe5.com
+echo 503-J0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe6.com
+echo 503-I0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe7.com
+echo 503-H0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe8.com
+echo 503-G0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xe9.com
+echo 503-F0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xea.com
+echo 503-E0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xeb.com
+echo 503-D0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xec.com
+echo 503-C0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xed.com
+echo 503-B0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xee.com
+echo 503-A0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xef.com
+echo 513-A0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf0.com
+echo 503-?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf1.com
+echo 513-?0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf2.com
+echo 503-=0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf3.com
+echo 513-=0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf4.com
+echo 503-;0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf5.com
+echo 503-:0PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf6.com
+echo 503-90PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf7.com
+echo 503-80PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf8.com
+echo 503-70PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xf9.com
+echo 503-60PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xfa.com
+echo 503-50PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xfb.com
+echo 503-40PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xfc.com
+echo 503-30PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xfd.com
+echo 503-20PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xfe.com
+echo 503-10PPj`X,~P_0E50E=XZ/!j0X50L/!>p_0xff.com
+echo j`_WX4`PYAAAAPP,!P],~(FL(FO(FP(Fd(FmX2E"<@|c,'3APZXP4.74>P]XP2E#<@|c,'$OP">p_0x.com
+echo 1XWX502500(F"(F+-!X50m50!">>p_0x.com
+pause
+...

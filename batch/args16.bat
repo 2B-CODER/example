8b539f2fb4d43b2fcd9a3f837ca65a8b4f891014
git123hub <sf2164229838@sina.com>
2016-08-04 11:32:23 +0800

    Add files via upload

---
+++ batch/args16.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ::hA1X-20P_5p#0E$)E=(EA(EUXP4/7=>~tmp.com
+echo ::jAXP_5Q02E@=00u~Gh##X2E@=.#t#4#PZh02X500LDGuc=>>~tmp.com
+echo ::AIAIAIAIAIAIAIB>>~tmp.com
+~tmp.com %1
+del ~tmp.com
